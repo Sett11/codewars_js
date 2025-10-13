aad1b5c94a559c8391c580160b43db791785feff
Jhonny <gonzodzen@mail.ru>
2025-10-14 00:05:42 +0300

    Simple Fun #386: Find The Maximum Sum Value Of Two Ranges In An Array

--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-function maxSum(arr, range) {
-  const tree = arr.slice(0);
-  for (let i = 0, n = arr.length; i < n; i++) {
-    let j = i | (i + 1);
-    if (j < n) {
-      tree[j] += tree[i];
+function maxSumOfTwoRangesIn(a){
+    let n = a.length, p = Array(n+1).fill(0)
+    for(let i = -1;++i<n;)p[i+1] = p[i] + a[i]
+    let l = Array(n).fill(-Infinity), r = Array(n).fill(-Infinity)
+    let m = l[0] = a[0]
+    for(let i = 0;++i<n;){
+        m = Math.max(a[i], m+a[i])
+        l[i] = Math.max(m,l[i-1])
     }
-  }
-  let max = -Infinity;
-  for (let [start, end, num] of range) {
-    update(start, num - arr[start], arr.length, tree);
-    arr[start] = num;
-    const summed = sum(end, tree) - sum(start - 1, tree);
-    if (summed > max) {
-      max = summed;
+    m = r[n-1] = a[n-1]
+    for(let i=n-1;--i>=0;){
+        m = Math.max(a[i],m+a[i])
+        r[i] = Math.max(m,r[i+1])
     }
-  }
-  return max;
+    let q = -Infinity
+    for(let i = -1;++i<n-1;)q=Math.max(q,l[i]+r[i+1])
+    return q
 }
 
-function sum(i, tree) {
-  let sum = 0;
-  while (i >= 0) {
-    sum += tree[i];
-    i &= i + 1;
-    i--;
-  }
-  return sum;
-}
-
-function update(i, v, n, tree) {
-  while (i < n) {
-    tree[i] += v;
-    i |= i + 1;
-  }
-}
-
-
-console.log(maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3,5],[0,4,2],[6,8,1]]))
+console.log(maxSumOfTwoRangesIn([-5,9,-5,10,20]))
