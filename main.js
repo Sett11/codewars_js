76030d2ed70bc73b689101f22cdf82d8a37207aa
Jhonny <gonzodzen@mail.ru>
2025-10-17 00:01:21 +0300

    3 Hoops Game

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-function countAndSort(a) {
-    let n = a.length, p = ~~(n / 2), r = {}
-    for(let i = -1; ++i < n;){
-        for(let j = -1; ++j < n;){
-            if(a[i][j] !== '*'){
-                let t = a[i][j].toLowerCase(), x = Math.abs(p - i), y = Math.abs(p - j)
-                if(r[t] === undefined)r[t] = [0, 0]
-                r[t][0] += Math.min(p-x+1,p-y+1) * (a[i][j]===t?1:2)
-                r[t][1] += 1 + (a[i][j] !== t)
+const prizeCounter = a => {
+    let u = new Set(), o = {'R':500, 'B':300, 'G':200}, t = 0
+    for(let i=-1;++i<a.length;){
+        if(!u.has(a[i])){
+            t += 100
+            x = a.slice(i,i+3)
+            if((x.length === 3) && new Set(x).size === 1){
+                u = new Set([a[i]])
+                t += o[a[i]] + 200
+                i += 2
             }
         }
     }
-    return Object.entries(r).sort((a, b) => (a[1][0]-b[1][0]) || (b[1][1] - a[1][1]) || a[0].localeCompare(b[0])).map(e=>e[0])
+    return t
 }
 
-console.log(countAndSort([
-         "z**",
-         "a**",
-         "**z"
-      ]))
+console.log(prizeCounter(['R', 'R', 'R', 'G', 'B', 'R', 'B', 'B', 'B', 'R']))
