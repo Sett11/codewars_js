3acc8065432dbdc1db44a030b1b03d8cb37c5ce2
Jhonny <gonzodzen@mail.ru>
2025-10-18 01:37:13 +0300

    Extending JavaScript Objects: Get First & Last Array Element

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-const prizeCounter = a => {
-    let u = new Set(), o = {'R':500, 'B':300, 'G':200}, t = 0
-    for(let i=-1;++i<a.length;){
-        if(!u.has(a[i])){
-            t += 100
-            x = a.slice(i,i+3)
-            if((x.length === 3) && new Set(x).size === 1){
-                u = new Set([a[i]])
-                t += o[a[i]] + 200
-                i += 2
-            }
-        }
-    }
-    return t
+Array.prototype.first = function() {
+    return this[0]
 }
 
-console.log(prizeCounter(['R', 'R', 'R', 'G', 'B', 'R', 'B', 'B', 'B', 'R']))
+Array.prototype.last = function() {
+    return this[this.length - 1]
+}
+
+console.log([1,2,3,4,5].first())
+console.log([1,2,3,4,5].last())
