d5643e93952b61a4ea149ebf1a12256cca5c0190
Jhonny <gonzodzen@mail.ru>
2025-10-24 00:08:51 +0300

    Celebrity Baby Names

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function getCharacters(obj, key, val) {
-    var foundCharacters = [];
-    for(let i of obj["characters"])if(i[key]&&i[key].toLowerCase()===val.toLowerCase())foundCharacters.push(i)
-    return foundCharacters
-  }
+function validName(a){
+  const l = a.length
+  if(!l)return "You must test at least one name."
+  if(l===1)return "Congratulations, you can choose any name you like!"
+  for(let i=1; i<l; i++)if(a[i][0].toLowerCase()!==a[i-1][a[i-1].length-1].toLowerCase())return "Back to the drawing board, your baby names are not compatible."
+  return "Congratulations, your baby names are compatible!"
+}
