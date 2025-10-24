1a7ee6d40a585f9fdda82ca1dc207882fb89bd0c
Jhonny <gonzodzen@mail.ru>
2025-10-25 00:04:51 +0300

    Noye's Fludde

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function validName(a){
-  const l = a.length
-  if(!l)return "You must test at least one name."
-  if(l===1)return "Congratulations, you can choose any name you like!"
-  for(let i=1; i<l; i++)if(a[i][0].toLowerCase()!==a[i-1][a[i-1].length-1].toLowerCase())return "Back to the drawing board, your baby names are not compatible."
-  return "Congratulations, your baby names are compatible!"
+function boatLoader(arr) {
+  let a = arr.filter(e=>typeof e === 'string')
+  let counts = a.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1
+    return acc
+  }, {});
+  let filtered = Object.entries(counts)
+    .filter(([key, value]) => value > 1)
+    .map(([key]) => key);
+  filtered.sort((a, b) => {
+    if (a.toLowerCase() === b.toLowerCase()) {
+      if (a === b) return 0;
+      if (a === a.toUpperCase() && b === b.toLowerCase()) return -1;
+      if (a === a.toLowerCase() && b === b.toUpperCase()) return 1;
+      return a.localeCompare(b);
+    }
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  })
+  return filtered.map(e => [e, e]);
 }
+
+console.log(boatLoader(['a','d','G','V','X','F','g','h','s','r','a','g','n','s','e','r','j','a','f','D','F','G','R','H','C','X','B','N','G','U','G','F','p','s','r','g','a']))
