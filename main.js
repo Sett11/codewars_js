68ecf9447136409305698a7cfb4558d4ec3c77d3
Jhonny <gonzodzen@mail.ru>
2025-10-27 00:08:26 +0300

    Cinemas in 2020

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function perc(minutes) {
-    if (typeof minutes !== 'number' || minutes < 1 || minutes > 100) return "invalid track time";
-    const bpm = 120;
-    const beatsPerBar = 4;
-    const barsPerMinute = bpm / beatsPerBar;
-    const totalBars = minutes * barsPerMinute;
-    const kicks = Math.round(totalBars * 4);
-    const hihats = Math.round(totalBars * 8);
-    const claps = Math.round(totalBars * 2);
-    return [`${kicks} kicks`, `${hihats} hihats`, `${claps} claps`];
+function maximumSeating(a) {
+  let c = 0
+  let f = x =>{
+    for(let i = 0; i < x.length; i++){
+      if(!x[i]&&!x[i+1]&&!x[i+2]&&!x[i-1]&&!x[i-2]){
+        x[i]=1
+        c++
+      }
+    }
+  }
+  f(a),f(a.slice().reverse())
+  return c
 }
-
-console.log(perc(3))
