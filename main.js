e9a7e8bf9a1dd268b3713a5eb8d271344dc1d989
Jhonny <gonzodzen@mail.ru>
2025-10-29 00:20:16 +0300

    Create 10x10 array and randomly fill each cell with "A", "B", "C", or "D"

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-Array.prototype.toDictionary = function(k, v=x=>x) {
-    const r = {}
-    for(let i of this)r[k(i)]=v(i)
-    return r
+const array10 = () => {
+  const result = Array(10).fill().map(() => Array(10));
+  const letters = ['A','B','C','D'];
+  for (let i = 0; i < 10; i++) {
+    const positions = [...Array(10).keys()].sort(() => Math.random() - 0.5).slice(0,4);
+    letters.forEach((letter, idx) => result[i][positions[idx]] = letter);
+    for (let j = 0; j < 10; j++) {
+      if (!result[i][j]) result[i][j] = letters[Math.floor(Math.random() * 4)];
+    }
   }
+  return result
+}
