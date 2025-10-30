94e035c7dd86584153c7e1e70e2fd6452a9dfead
Jhonny <gonzodzen@mail.ru>
2025-10-31 00:10:36 +0300

    Blood-Alcohol Content

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function was_package_received_yesterday(tz_from, tz_to, start, duration) {
-  const time_diff = tz_to - tz_from;
-  const receive_time = start + duration + time_diff;
-  return receive_time < 0;
+function bloodAlcoholContent(drinks, weight, sex, time){
+  const A = drinks.ounces * drinks.abv;
+  const W = weight;
+  const r = sex === "male" ? 0.73 : 0.66;
+  const H = time;
+  
+  let bac = (A * 5.14 / W * r) - 0.015 * H;
+  bac = Math.round(bac * 10000) / 10000;
+  return bac < 0 ? 0 : bac;
 }
