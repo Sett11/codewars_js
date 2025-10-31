a128b29f34e20bce98710cb363748ce1cee52953
Jhonny <gonzodzen@mail.ru>
2025-11-01 00:05:01 +0300

    Loop Detector

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function bloodAlcoholContent(drinks, weight, sex, time){
-  const A = drinks.ounces * drinks.abv;
-  const W = weight;
-  const r = sex === "male" ? 0.73 : 0.66;
-  const H = time;
-  
-  let bac = (A * 5.14 / W * r) - 0.015 * H;
-  bac = Math.round(bac * 10000) / 10000;
-  return bac < 0 ? 0 : bac;
+function hasLoop(indices) {
+    if (indices.length === 0) return false;
+    
+    const visited = new Set();
+    let current = 0;
+    
+    while (true) {
+        if (visited.has(current)) return true;
+        
+        visited.add(current);
+        const next = indices[current];
+        if (next === undefined || next >= indices.length) return false;
+        
+        current = next;
+    }
 }
