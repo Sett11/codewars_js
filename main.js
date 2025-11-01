b4879e69473a22771681dcbc34981ec0c1c6c4fb
Jhonny <gonzodzen@mail.ru>
2025-11-02 00:09:39 +0300

    Slope of a Line

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-function hasLoop(indices) {
-    if (indices.length === 0) return false;
-    
-    const visited = new Set();
-    let current = 0;
-    
-    while (true) {
-        if (visited.has(current)) return true;
-        
-        visited.add(current);
-        const next = indices[current];
-        if (next === undefined || next >= indices.length) return false;
-        
-        current = next;
+function getSlope(p1, p2) {
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+    if (x1 === x2) {
+      return null;
     }
-}
+    return (y2 - y1) / (x2 - x1);
+  }
