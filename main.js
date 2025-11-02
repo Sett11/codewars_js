0dadc3e06d8a862b9be2761d3a407f89631a9e1e
Jhonny <gonzodzen@mail.ru>
2025-11-03 00:02:24 +0300

    Localize The Barycenter of a Triangle

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-function getSlope(p1, p2) {
-    const [x1, y1] = p1;
-    const [x2, y2] = p2;
-    if (x1 === x2) {
-      return null;
-    }
-    return (y2 - y1) / (x2 - x1);
-  }
+function barTriang(p1, p2, p3){
+  const xO = (p1[0] + p2[0] + p3[0]) / 3;
+  const yO = (p1[1] + p2[1] + p3[1]) / 3;
+  return [Number(xO.toFixed(4)), Number(yO.toFixed(4))];
+}
