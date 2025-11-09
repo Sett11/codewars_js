2c29ff0000dc33487db53191548edc89de4fedec
Jhonny <gonzodzen@mail.ru>
2025-11-10 00:04:42 +0300

    [BUG] XCOM-388: Mass spectrometer crashes

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-function qf(donations, pool) {
-  let received = [];
-  let weights = [];
-  let totalWeight = 0;
-  
-  for (let i = 0; i < donations.length; i++) {
-    let projectDonations = donations[i];
-    let sumOfRoots = 0;
-    
-    for (let j = 0; j < projectDonations.length; j++) {
-      sumOfRoots += Math.sqrt(projectDonations[j]);
+const Spectrometer = {
+  getHeaviest: function(atomicMasses) {
+    // Handling empty sample - return 0 for calibration
+    if (!atomicMasses || atomicMasses.length === 0) {
+      return 0;
     }
     
-    let weight = Math.pow(sumOfRoots, 2);
-    weights.push(weight);
-    totalWeight += weight;
-  }
-  
-  for (let i = 0; i < weights.length; i++) {
-    let funding = pool * (weights[i] / totalWeight);
-    received.push(Math.round(funding));
-  }
-  
-  return received;
-}
+    let maxMass = atomicMasses[0];
+    for (let i = 1; i < atomicMasses.length; i++) {
+      if (atomicMasses[i] > maxMass) {
+        maxMass = atomicMasses[i];
+      }
+    }
+    return maxMass;
+  }  
+};
+
+console.log(Spectrometer.getHeaviest([1, 2, 3, 4, 5]));
+console.log(Spectrometer.getHeaviest([]));
+console.log(Spectrometer.getHeaviest([10, 20, 30, 40, 50]));
+console.log(Spectrometer.getHeaviest([100, 200, 300, 400, 500]));
+console.log(Spectrometer.getHeaviest([1000, 2000, 3000, 4000, 5000]));
+console.log(Spectrometer.getHeaviest([10000, 20000, 30000, 40000, 50000]));
+console.log(Spectrometer.getHeaviest([100000, 200000, 300000, 400000, 500000]));
+console.log(Spectrometer.getHeaviest([1000000, 2000000, 3000000, 4000000, 5000000]));
+console.log(Spectrometer.getHeaviest([10000000, 20000000, 30000000, 40000000, 50000000]));
