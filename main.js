ca515a6855a46062ab400b9f82f2b47479f221d2
Jhonny <gonzodzen@mail.ru>
2025-11-11 00:16:11 +0300

    Plan Your Dream Wedding

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-const Spectrometer = {
-  getHeaviest: function(atomicMasses) {
-    // Handling empty sample - return 0 for calibration
-    if (!atomicMasses || atomicMasses.length === 0) {
-      return 0;
+function maxWeddingCost(C, r, S, T, W) {
+    const rDecimal = r / 100;
+    const monthsBefore = W * 12;
+    const monthsAfter = (T - W) * 12;
+    
+    function futureValueAnnuity(C, r, n) {
+        const factor = Math.pow(1 + r, n);
+        return C * (factor - 1) / r;
     }
     
-    let maxMass = atomicMasses[0];
-    for (let i = 1; i < atomicMasses.length; i++) {
-      if (atomicMasses[i] > maxMass) {
-        maxMass = atomicMasses[i];
-      }
+    const FV_before = futureValueAnnuity(C, rDecimal, monthsBefore);
+    const FV_after = futureValueAnnuity(C, rDecimal, monthsAfter);
+    const factorAfter = Math.pow(1 + rDecimal, monthsAfter);
+    
+    if (FV_after >= S) {
+        return FV_before;
+    } else {
+        return FV_before - (S - FV_after) / factorAfter;
     }
-    return maxMass;
-  }  
-};
-
-console.log(Spectrometer.getHeaviest([1, 2, 3, 4, 5]));
-console.log(Spectrometer.getHeaviest([]));
-console.log(Spectrometer.getHeaviest([10, 20, 30, 40, 50]));
-console.log(Spectrometer.getHeaviest([100, 200, 300, 400, 500]));
-console.log(Spectrometer.getHeaviest([1000, 2000, 3000, 4000, 5000]));
-console.log(Spectrometer.getHeaviest([10000, 20000, 30000, 40000, 50000]));
-console.log(Spectrometer.getHeaviest([100000, 200000, 300000, 400000, 500000]));
-console.log(Spectrometer.getHeaviest([1000000, 2000000, 3000000, 4000000, 5000000]));
-console.log(Spectrometer.getHeaviest([10000000, 20000000, 30000000, 40000000, 50000000]));
+}
