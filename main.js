8b3c9f2f2bf466413db2e79164414e80d0878ede
Jhonny <gonzodzen@mail.ru>
2025-11-14 00:08:25 +0300

    Scott Orderings and more

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-const gridPosition = (name, position) => {
-  if (position === 1) {
-    return `${name} starts from pole position`;
-  }
-  
-  // Calculate row number (1-based)
-  const row = Math.ceil(position / 3);
-  
-  // Calculate position within row (0: top, 1: middle, 2: bottom)
-  const positionInRow = (position - 1) % 3;
-  
-  let positionText;
-  switch (positionInRow) {
-    case 0:
-      positionText = "top";
-      break;
-    case 1:
-      positionText = "middle";
-      break;
-    case 2:
-      positionText = "bottom";
-      break;
-  }
-  
-  let rowText;
-  if (row === 1) {
-    rowText = "front row";
-  } else {
-    // Add ordinal suffix
-    const suffixes = ["th", "st", "nd", "rd"];
-    const suffix = row % 100 >= 11 && row % 100 <= 13 ? "th" : suffixes[row % 10] || "th";
-    rowText = `${row}${suffix} row`;
-  }
-  
-  return `${name} starts from the ${positionText} of the ${rowText}`;
-};
+const LT = lt => eq => gt => lt
+const EQ = lt => eq => gt => eq
+const GT = lt => eq => gt => gt
+
+// do not change this function //
+function compare(x,y) {
+  if ( x < y )
+    return LT;
+  else if ( x > y )
+    return GT;
+  else
+    return EQ;
+}
+
+function sorted([...xs]) {
+  return xs.sort( (a,b) => compare(a,b)(-1)(0)(1) );
+}
+
+function maximum(xs) {
+  return xs.reduce( (accumulator,currentValue) => 
+    compare(accumulator,currentValue)(currentValue)(accumulator)(accumulator)
+  );
+}
+
+const UNIT = _ => _
+
+const RED    = red => orange => yellow => green => blue => purple => red
+const ORANGE = red => orange => yellow => green => blue => purple => orange
+const YELLOW = red => orange => yellow => green => blue => purple => yellow
+const GREEN  = red => orange => yellow => green => blue => purple => green
+const BLUE   = red => orange => yellow => green => blue => purple => blue
+const PURPLE = red => orange => yellow => green => blue => purple => purple
