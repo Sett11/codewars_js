445d42cdda761a87861db5b3f96ae8497115b693
Jhonny <gonzodzen@mail.ru>
2025-11-15 00:26:26 +0300

    Minimum Percentage of Visitors that Ate All Foods

--- main.js
+++ main.js
@@ -1,32 +1,5 @@
-const LT = lt => eq => gt => lt
-const EQ = lt => eq => gt => eq
-const GT = lt => eq => gt => gt
-
-// do not change this function //
-function compare(x,y) {
-  if ( x < y )
-    return LT;
-  else if ( x > y )
-    return GT;
-  else
-    return EQ;
+function minimumPercentage(foods) {
+  const sum = foods.reduce((a, b) => a + b, 0);
+  const minAll = sum - 100 * (foods.length - 1);
+  return Math.max(0, minAll);
 }
-
-function sorted([...xs]) {
-  return xs.sort( (a,b) => compare(a,b)(-1)(0)(1) );
-}
-
-function maximum(xs) {
-  return xs.reduce( (accumulator,currentValue) => 
-    compare(accumulator,currentValue)(currentValue)(accumulator)(accumulator)
-  );
-}
-
-const UNIT = _ => _
-
-const RED    = red => orange => yellow => green => blue => purple => red
-const ORANGE = red => orange => yellow => green => blue => purple => orange
-const YELLOW = red => orange => yellow => green => blue => purple => yellow
-const GREEN  = red => orange => yellow => green => blue => purple => green
-const BLUE   = red => orange => yellow => green => blue => purple => blue
-const PURPLE = red => orange => yellow => green => blue => purple => purple
