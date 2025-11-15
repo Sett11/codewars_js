2a48d5a5680247443d0adf3ddd35b0c59fa718b3
Jhonny <gonzodzen@mail.ru>
2025-11-16 00:02:50 +0300

    Natural Emptiness

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function minimumPercentage(foods) {
-  const sum = foods.reduce((a, b) => a + b, 0);
-  const minAll = sum - 100 * (foods.length - 1);
-  return Math.max(0, minAll);
+function repSet(n) {
+  let s = [];
+  for (let i = 0; i < n; i++) s = [...s, s];
+  return s;
 }
