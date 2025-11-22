f87feee07380afee0f9ddf671b907f24a29a5082
Jhonny <gonzodzen@mail.ru>
2025-11-23 01:00:10 +0300

    99 Bottles of Beer

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-function crossProduct(v1, v2) {
+var sing = function () {
+  let lyrics = [];
+  for (let i = 99; i > 0; i--) {
+    let bottle = i === 1 ? "bottle" : "bottles";
+    let nextBottle = i - 1 === 1 ? "bottle" : "bottles";
+    let nextCount = i - 1 === 0 ? "no more" : i - 1;
+    
+    lyrics.push(`${i} ${bottle} of beer on the wall, ${i} ${bottle} of beer.`);
+    lyrics.push(`Take one down and pass it around, ${nextCount} ${nextBottle} of beer on the wall.`);
+  }
   
-  const x = v1.y * v2.z - v1.z * v2.y;
-  const y = v1.z * v2.x - v1.x * v2.z;
-  const z = v1.x * v2.y - v1.y * v2.x;
+  lyrics.push("No more bottles of beer on the wall, no more bottles of beer.");
+  lyrics.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
   
-  return new Vector(x, y, z);
-}
+  return lyrics;
+};
