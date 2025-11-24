2a737119a70c4da4d2d4c366769d49d1ef1cd2eb
Jhonny <gonzodzen@mail.ru>
2025-11-25 00:05:13 +0300

    Deodorant Evaporator

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-var sing = function () {
-  let lyrics = [];
-  for (let i = 99; i > 0; i--) {
-    let bottle = i === 1 ? "bottle" : "bottles";
-    let nextBottle = i - 1 === 1 ? "bottle" : "bottles";
-    let nextCount = i - 1 === 0 ? "no more" : i - 1;
+function evaporator(content, evapPerDay, threshold) {
+    let days = 0;
+    let currentContent = 100;
+    const thresholdPercent = threshold;
     
-    lyrics.push(`${i} ${bottle} of beer on the wall, ${i} ${bottle} of beer.`);
-    lyrics.push(`Take one down and pass it around, ${nextCount} ${nextBottle} of beer on the wall.`);
-  }
-  
-  lyrics.push("No more bottles of beer on the wall, no more bottles of beer.");
-  lyrics.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
-  
-  return lyrics;
-};
+    while (currentContent > thresholdPercent) {
+        currentContent -= currentContent * (evapPerDay / 100);
+        days++;
+    }
+    
+    return days;
+}
