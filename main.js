7c9c0730f3c026b3e4dc9d36d2d841f26120eb5a
Jhonny <gonzodzen@mail.ru>
2025-11-28 00:30:45 +0300

    Area of an arrow

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function unluckyDays(year){
-    let blackFridays = 0;
-    
-    for (let month = 0; month < 12; month++) {
-      const thirteenth = new Date(year, month, 13);
-      if (thirteenth.getDay() === 5) {
-        blackFridays++;
-      }
-    }
-    
-    return blackFridays;
-  }
+function arrowArea(a,b) {
+  return (a * b) / 4;
+}
