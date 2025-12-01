37d9fd4ac6166799c5e8747eaec7888e4081d5be
Jhonny <gonzodzen@mail.ru>
2025-12-02 00:07:28 +0300

    Rubik's Cube Art

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function redKnight(N, P) {
-    const color = (N + P) % 2 === 0 ? "White" : "Black";
-    return [color, 2 * P];
+function cube(n) {
+  let result = [];
+  
+  for (let i = 0; i < n; i++) {
+    let spaces = ' '.repeat(n - i - 1);
+    let slashes = '/\\'.repeat(i + 1);
+    let underscores = '_\\'.repeat(n);
+    result.push(spaces + slashes + underscores);
   }
+  for (let i = 0; i < n; i++) {
+    let spaces = ' '.repeat(i);
+    let slashes = '\\/'.repeat(n - i);
+    let underscores = '_/'.repeat(n);
+    result.push(spaces + slashes + underscores);
+  }
+  
+  return result.join('\n');
+}
